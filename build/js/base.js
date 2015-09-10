df0af2b6d8fee53fa776aa0b068530a06c1c2f52
Bogdan <bogdan@Bogdans-MacBook-Pro-15.local>
2015-09-10 20:42:59 +0200

    clear .. js

--- build/js/base.js
+++ build/js/base.js
@@ -1441,105 +1441,113 @@
     });
   }
 }.call(this));
-;  /* =======================================
-   * Init js
-   * =======================================
-   */
-  ;
-  console.log('--');
-  console.log('Hello visitor & wellcome to afterwork space.');
-  console.log('contact us @ contact@afterworkspace.com');
-  console.log('--');
+;/* =======================================
+ * Init js
+ * =======================================
+ */
 
-  /* =======================================
-   * DOM ready
-   * =======================================
-   */
+console.log('--');
+console.log('Hello visitor & wellcome to afterwork space.');
+console.log('contact us @ contact@afterworkspace.com');
+console.log('--');
+
+/* =======================================
+ * DOM ready
+ * =======================================
+ */
 $( document ).ready(function(){
-  //  console.info( "DOM ready..." );
+ console.info( "DOM ready..." );
 
-  //find in html el class name -touch-
-  var classTouch = $("html").hasClass("touch");
-  // console.log("html - has class touch (mobile device): " + classTouch);
+//find in html el class name -touch-
+var classTouch = $("html").hasClass("touch");
+// console.log("html - has class touch (mobile device): " + classTouch);
 });
 
-  /* =======================================
-   * On win load
-   * =======================================
-   */
-  $(window).load(function() {
-    console.log('window loaded..');
+/* =======================================
+ * On win load
+ * =======================================
+ */
+$(window).load(function() {
+  console.log('window loaded..');
 
-    if (classTouch) { //this is mobile device
-      // remove vide el.
-      $('.video-background-container').find('video').remove();
-      // set img insted video
-      $('.video-background-container').css("background-image", "url('img/ui/cover-photo-light.jpg')");
-      $('.video-background-container').css("background-size", "cover");
-      // $(".ui-container").css("top", "0px");
-      // $(".hp__intro").css("position", "relative");
-      // $('.hp__intro').css("display", "block");
-      // $('#preloader').addClass('start-opacity');
+   $('body').addClass('preloader-done');
+    $('.hp__intro').css("display", "block");
+   //remove AW
+   $('#preloader').addClass('start-opacity');
+
+  if (classTouch) { //this is mobile device
+    // remove vide el.
+    // $('.video-background-container').find('video').remove();
+    // set img insted video
+    // $('.video-background-container').css("background-image", "url('img/ui/cover-photo-light.jpg')");
+    // $('.video-background-container').css("background-size", "cover");
+    // $(".ui-container").css("top", "0px");
+    // $(".hp__intro").css("position", "relative");
+    // $('.hp__intro').css("display", "block");
+    // $('#preloader').addClass('start-opacity');
+  } else {
+    // $( '#preloader' ).fadeOut( 1000, function() {
+    // $('body').addClass('preloader-done');
+    // $('.hp__intro').css("display", "block");
+    //remove AW
+    // $('#preloader').addClass('start-opacity');
+  }
+
+});
+
+/* =======================================
+ * onScroll
+ * =======================================
+ */
+// $('.l-header').addClass('hide');
+
+
+
+  $(window).on('scroll', function() {
+
+    if (!classTouch) {
+
+      var countT = 0;
+    //  Header menu show/hide
+    if ($(window).scrollTop() > 55) {
+      $('.l-header').addClass('hide');
+      $('.ui-container').removeClass('ui--down');
+      // set 1st time only
+
+      var bodyTop = $('body').scrollTop();
+      console.log(bodyTop);
+
+      if (countT == 0) {
+        $('.ui-container').addClass('ui--up');
+        $('.ui-container').css('top', bodyTop);
+      }
+      countT = 1;
+
     } else {
-      // $( '#preloader' ).fadeOut( 1000, function() {
-      $('body').addClass('preloader-done');
-      // $('.hp__intro').css("display", "block");
-      //remove AW
-      $('#preloader').addClass('start-opacity');
+      countT = 0; //reset
+      $('.l-header').removeClass('hide');
+      $('.ui-container').removeClass('ui--up');
+      $('.ui-container').addClass('ui--down');
+      $('.ui-container').css('top', 900);
     }
 
-  });
-
-  /* =======================================
-   * onScroll
-   * =======================================
-   */
-  // $('.l-header').addClass('hide');
-
-  if (!classTouch) {
-
-    var countT = 0;
-
-    $(window).on('scroll', function() {
-      //  Header menu show/hide
-      if ($(window).scrollTop() > 55) {
-        $('.l-header').addClass('hide');
-        $('.ui-container').removeClass('ui--down');
-        // set 1st time only
-
-        var bodyTop = $('body').scrollTop();
-        console.log(bodyTop);
-
-        if (countT == 0) {
-          $('.ui-container').addClass('ui--up');
-          $('.ui-container').css('top', bodyTop);
-        }
-        countT = 1;
-
-      } else {
-        countT = 0; //reset
-        $('.l-header').removeClass('hide');
-        $('.ui-container').removeClass('ui--up');
-        $('.ui-container').addClass('ui--down');
-        $('.ui-container').css('top', 900);
-      }
-    });
   } else {
     $('.l-header').addClass('hide');
   }
-
-  /* =======================================
-   * WOW - anima
-   * =======================================
-   */
-  wow = new WOW({
-    animateClass: 'animated',
-    offset: 50,
-    mobile: false,
-    callback: function(box) {
-      // console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
-    }
   });
-  wow.init();
+
+/* =======================================
+ * WOW - anima
+ * =======================================
+ */
+wow = new WOW({
+  animateClass: 'animated',
+  offset: 50,
+  mobile: false,
+  callback: function(box) {
+    console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+  }
+});
+wow.init();
 
 //# sourceMappingURL=base.js.map
