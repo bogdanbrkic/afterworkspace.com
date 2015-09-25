02773d8e37e94c9bec48d9a69e930d3103da6060
Bogdan <bogdanbrkic@Bogdans-MacBook-Pro.local>
2015-09-25 15:54:02 +0200

    scroll page - smooth

--- js/main.js
+++ js/main.js
@@ -54,17 +54,13 @@ $(window).load(function() {
 
 });
 
-
-  $(window).load(function(){
-    $(".content").mCustomScrollbar();
-  });
 /* =======================================
  * onScroll
  * =======================================
  */
-$('.l-header').addClass('hide');
+// $('.l-header').addClass('hide');
 
-  var countT = 0;
+  // var countT = 0;
 
   $(window).on('scroll', function() {
 
@@ -73,30 +69,52 @@ $('.l-header').addClass('hide');
     //  Header menu show/hide
     if ($(window).scrollTop() > 55) {
       $('.l-header').addClass('hide');
-      $('.ui-container').removeClass('ui--down');
+      // $('.ui-container').removeClass('ui--down');
       // set 1st time only
 
-      var bodyTop = $('body').scrollTop();
-      console.log(bodyTop);
+      // var bodyTop = $('body').scrollTop();
+      // console.log(bodyTop);
 
-      if (countT == 0) {
-        $('.ui-container').addClass('ui--up');
-        $('.ui-container').css('top', bodyTop);
-      }
-      countT = 1;
+      // if (countT == 0) {
+      //   $('.ui-container').addClass('ui--up');
+      //   $('.ui-container').css('top', bodyTop);
+      // }
+      // countT = 1;
 
     } else {
-      countT = 0; //reset
+      // countT = 0; //reset
       $('.l-header').removeClass('hide');
-      $('.ui-container').removeClass('ui--up');
-      $('.ui-container').addClass('ui--down');
-      $('.ui-container').css('top', 900);
+      // $('.ui-container').removeClass('ui--up');
+      // $('.ui-container').addClass('ui--down');
+      // $('.ui-container').css('top', 900);
     }
 
   } else {
     $('.l-header').addClass('hide');
   }
   });
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+  var delta = 0;
+  if (event.wheelDelta) delta = event.wheelDelta / 120;
+  else if (event.detail) delta = -event.detail / 3;
+
+  handle(delta);
+  if (event.preventDefault) event.preventDefault();
+  event.returnValue = false;
+}
+
+function handle(delta) {
+  var time = 1000;
+  var distance = 180;
+
+  $('html, body').stop().animate({
+    scrollTop: $(window).scrollTop() - (distance * delta)
+  }, time );
+}
 
 /* =======================================
  * WOW - anima
